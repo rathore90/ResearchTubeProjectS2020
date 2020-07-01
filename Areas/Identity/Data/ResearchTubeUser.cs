0a13f75ae3186d959f0cc08cc4d4b1a22de46c9a
rathore90 <rathore90@hotmail.com>
2020-07-01 16:40:18 -0700

    Add project files.

---
+++ Areas/Identity/Data/ResearchTubeUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ResearchTube.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ResearchTubeUser class
+    public class ResearchTubeUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Interest { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResearchTube.Areas.Identity.Data;
+using ResearchTube.Data;
+
+[assembly: HostingStartup(typeof(ResearchTube.Areas.Identity.IdentityHostingStartup))]
+namespace ResearchTube.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ResearchTubeDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ResearchTubeDbContextConnection")));
+
+                services.AddDefaultIdentity<ResearchTubeUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ResearchTubeDbContext>();
+            });
+        }
+    }
+}

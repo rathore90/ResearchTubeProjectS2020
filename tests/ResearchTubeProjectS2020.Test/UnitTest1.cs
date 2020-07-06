6952b3374ee552581315ecc9e4545bacbf2d0250
Marcus Tam <45529250+succram@users.noreply.github.com>
2020-07-06 18:01:27 -0400

    xUnit Tests

---
+++ tests/ResearchTubeProjectS2020.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ResearchTubeProjectS2020.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

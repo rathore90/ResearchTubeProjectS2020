a70ead04a4f1bf10b681a912812bbae1fa414223
Zhaoyang98 <Zhaoyang98@users.noreply.github.com>
2020-07-13 21:36:00 -0700

    Uploading is functional if you upload from local to project wwwroot/Videos.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ResearchTube.Models;
 
+
 namespace ResearchTube.Controllers
 {
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult _LoginPartial() 
+        {
+            return View();
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -37,6 +47,39 @@ namespace ResearchTube.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        [HttpPost]
+        public IActionResult upload([FromServices] IWebHostEnvironment environment)
+        {
+            
+           var files = Request.Form.Files;
+            string webRootPath = environment.WebRootPath;
+            string filePath = webRootPath + "/Videos/";
+            string fullPath = "";
+
+            string fileName = Path.GetFileName(files[0].FileName);
+            string fileExtension = Path.GetExtension(fileName).ToLower(); 
+
+            if (fileExtension == ".mp4" || fileExtension == ".webm" || fileExtension == ".MPEG")
+            {
+                string saveName = DateTime.Now.ToString("yyyyMMddhhmmssffff") + fileExtension; 
+                fullPath = Path.Combine(filePath, saveName);
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                using (var stream = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    files[0].CopyTo(stream);
+                }
+                return Content("ok");
+            }
+            //uploadPath = "/Uploads/Images/" + folder + "/" + saveName;
+
+            return Content("Upload format error！");
         }
     }
 }

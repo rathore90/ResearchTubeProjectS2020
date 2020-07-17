49f482db2b71e1952a3e235413deff6032a042d8
zhewen Zhang <41249951+ZhewenZhang2333@users.noreply.github.com>
2020-07-16 17:39:15 -0700

    home page of profile complete

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -8,7 +8,7 @@ namespace ResearchTube.Areas.Identity.Pages.Account.Manage
 {
     public static class ManageNavPages
     {
-        public static string Index => "Index";
+        //public static string Index => "Index";
 
         public static string Email => "Email";
 
@@ -26,7 +26,7 @@ namespace ResearchTube.Areas.Identity.Pages.Account.Manage
 
         //public static string LibraryData => "LibraryData";
 
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        //public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 

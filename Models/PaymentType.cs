95cf92fc07926a2169d4109b03649810b298f020
rathore90 <rathore90@hotmail.com>
2020-07-21 13:31:04 -0700

    Fix Payment and Plantype relationship

---
+++ Models/PaymentType.cs
@@ -0,0 +1,11 @@
+﻿namespace ResearchTube.Models
+{
+    public class PaymentType
+    {
+        public string PaymentTypeId { get; set; }
+        public string AccessLevel { get; set; }
+
+        public int CurrentPaymentId { get; set; }
+        public Payment Payment { get; set; }
+    }
+}

942f64873cca3273a35678e99bfd3b09f3d451e8
YueLiz <1551472043@qq.com>
2020-07-22 16:16:44 -0700

    Unit Test View Model Validation

---
+++ ResearchTube.Test/TestChangePassword.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+using static ResearchTube.Areas.Identity.Pages.Account.Manage.ChangePasswordModel;
+
+namespace ResearchTube.Test
+{
+    public class TestChangePassword
+    {
+
+        [Fact]
+        public void TestPasswordLength()
+        {
+            var inputModel = new InputModel
+            {
+                NewPassword = "123"
+            };
+
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(inputModel, new ValidationContext(inputModel), validationResults, true);
+
+            Assert.Contains(validationResults, ValidationResult => ValidationResult.ErrorMessage == "The New password must be at least 6 and at max 100 characters long.");
+        }
+
+        [Fact]
+        public void TestConfirmPassword()
+        {
+            var inputModel = new InputModel
+            {
+                NewPassword = "123Qw.",
+                ConfirmPassword = "123QQw"
+            };
+
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(inputModel, new ValidationContext(inputModel), validationResults, true);
+
+            Assert.Contains(validationResults, ValidationResult => ValidationResult.ErrorMessage == "The new password and confirmation password do not match.");
+        }
+    }
+}
